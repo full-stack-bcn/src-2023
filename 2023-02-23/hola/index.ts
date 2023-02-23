892cfec0c51c6d4e7ff8408ced564b3346efa70e
Pau Fernandez <pau.fernandez@upc.edu>
2023-02-23 20:27:14 +0100

    Ejemplo de interpolación de Strings

--- 2023-02-23/hola/index.ts
+++ 2023-02-23/hola/index.ts
@@ -9,4 +9,10 @@ console.log(a, s, b);
 console.log(frase);
 console.log(frase2);
 
+let total = 100;
+console.log(`El total es ${total * 10}, eso es.`);
+console.log("El total es ${total}.");
 
+console.log(`<div>El total es <code>${total}</code></div>`);
+
+console.log(typeof total);
