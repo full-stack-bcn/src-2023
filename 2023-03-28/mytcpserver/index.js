77ee0e214c5ed400cbef3d8be26b8636e71821f7
alicevm <alice@vm>
2023-03-30 16:34:03 +0000

    minor changes

--- 2023-03-28/mytcpserver/index.js
+++ 2023-03-28/mytcpserver/index.js
@@ -3,10 +3,18 @@ const server = net.createServer();
 
 server.on("connection", handleConnection);
 
-server.listen(12345, "0.0.0.0", () => {
+server.listen(5000, "0.0.0.0", () => {
     console.log(`server listening ${JSON.stringify(server.address())}`);
 })
 
 function handleConnection(socket) {
   console.log(`Nuevo cliente desde ${socket.remoteAddress}:${socket.remotePort}`);
+    socket.setEncoding("utf8");
+    socket.on("data", onConnData);
+
+    function onConnData(data) {
+        socket.write(data.toUpperCase());
+    }
+
+
 }
