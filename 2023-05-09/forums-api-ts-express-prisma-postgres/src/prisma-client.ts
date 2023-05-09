a08e17c1d4713ef0b1f9235ab50d3a5254f48be0
Pau Fernandez <pau.fernandez@upc.edu>
2023-05-09 21:32:05 +0200

    Documento mejor prisma-client.ts

--- 2023-05-09/forums-api-ts-express-prisma-postgres/src/prisma-client.ts
+++ 2023-05-09/forums-api-ts-express-prisma-postgres/src/prisma-client.ts
@@ -1,2 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-export default new PrismaClient();
+
+console.log("Creando Prisma Client");
+
+const prisma = new PrismaClient();
+export default prisma;
