86bcc04bc88f025b67320d63888d78e12c6cc53a
Pau Fernandez <pau.fernandez@upc.edu>
2023-06-06 19:20:36 +0200

    Versión inicial

---
+++ 2023-06-06/next1/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="p-3">
+      <h1 className="font-bold text-2xl">Este es mi primer proyecto NextJS (13.2)</h1>
+    </main>
+  )
+}
